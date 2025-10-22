d6fdd9fad20130d4998e18d2722e19946bd096a5
LeAndre <187464993+lecanact@users.noreply.github.com>
2025-10-22 18:12:17 -0400

    move everything to a .js file for organization

---
+++ 404.js
@@ -0,0 +1,11 @@
+const page = window.location.pathname.split('/')[1];
+    const returnDiv = document.getElementsByName('return')[0];
+    const returnredirect = document.querySelector('.btn');
+
+    if (page == "dashboard") {
+      returnDiv.innerText = "Back To Dashboard";
+      returnredirect.href = "https://thrd.xyz/dashboard";
+    } else {
+      returnDiv.innerText = "Back To Home";
+      returnredirect.href = "https://thrd.xyz";
+    }
